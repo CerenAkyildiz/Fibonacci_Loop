cab58ede42d295cef7319179ee9892cc22359828
Ceren <akyildiz_ceren@hotmail.com>
2015-02-22 00:04:57 +0200

    Fibonacci Sayıları
    Fibonacci sayılarının döngüsel şekilde hesaplanması

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Loop
+{
+    class Program
+    {
+        public static Stopwatch watch = new Stopwatch();
+
+        public static long fibonacciLoop(long girdi)
+        {
+
+            long fib1 = 1;
+            long fib2 = 1;
+            long gecici = 0;
+            while (2 < girdi)
+            {
+                gecici = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = gecici;
+                girdi = girdi - 1;
+            }
+            return gecici;
+
+        }
+        static void Main(string[] args)
+        {
+            long deger = 0;
+            Console.WriteLine("Kaçıncı fibonacci sayısını hesaplamak istiyorsunuz: ");
+            deger = Convert.ToInt32(Console.ReadLine());
+            watch.Start();
+            Console.WriteLine("{0} . Fibonacci: {1}", deger, fibonacciLoop(deger));
+            watch.Stop();
+            Console.WriteLine("Gecen sure: {0} ", watch.Elapsed.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
